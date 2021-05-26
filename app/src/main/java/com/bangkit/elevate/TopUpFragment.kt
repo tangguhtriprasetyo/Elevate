60094427522d4f89e5f4edfa415456f2033bc0a3
Danny <dannyamitayus90@gmail.com>
2021-05-27 03:18:57 +0700

    + Home kurang isi toolbar + Front end Profile -> Top Up -> Withdrawn == Done

---
+++ app/src/main/java/com/bangkit/elevate/TopUpFragment.kt
@@ -0,0 +1,15 @@
+package com.bangkit.elevate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TopUpFragment : Fragment(R.layout.fragment_top_up) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/bangkit/elevate/ui/dashboard/profile/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/elevate/ui/dashboard/profile/ProfileFragment.kt
@@ -4,41 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.elevate.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {
+
+class ProfileFragment : Fragment(){
 
     private lateinit var profileViewModel: ProfileViewModel
-    private var _binding: FragmentProfileBinding? = null
+    private lateinit var _binding: FragmentProfileBinding
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        _binding.ProfileTopUp.setOnClickListener{
+//            val mCategoryFragment = TopUpFragment()
+//            val mFragmentManager = fragmentManager
+//            mFragmentManager?.beginTransaction()?.apply {
+//                replace(R.id.ProfileFrag, mCategoryFragment, TopUpFragment::class.java.simpleName)
+//                addToBackStack(null)
+//                commit()
+//            }
+        }
+//        val textView: TextView = binding.textNotifications
+//        profileViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }

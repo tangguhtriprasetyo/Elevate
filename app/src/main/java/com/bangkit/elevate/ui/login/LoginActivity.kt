815d9346bbf83926e3051357ffa6073d5254ae7b
Tangguh Tri Prasetyo <tangguhtriprasetyo@gmail.com>
2021-05-27 02:58:40 +0700

    Login with Google Done

--- app/src/main/java/com/bangkit/elevate/ui/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/elevate/ui/login/LoginActivity.kt
@@ -5,9 +5,13 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.bangkit.elevate.databinding.ActivityLoginBinding
+import com.bangkit.elevate.ui.dashboard.MainActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -16,27 +20,48 @@ import com.google.android.gms.common.api.ApiException
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loginViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(LoginViewModel::class.java)
         initGoogleSignInClient()
+
 
         val resultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
-                    // There are no request codes
+                    showLoading(true)
                     val data: Intent? = result.data
                     val task = GoogleSignIn.getSignedInAccountFromIntent(data)
                     try {
                         // Google Sign In was successful, authenticate with Firebase
                         val account = task.getResult(ApiException::class.java)!!
                         Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-//                        firebaseAuthWithGoogle(account.idToken!!)
-
+                        loginViewModel.signInWithGoogle(account.idToken!!)
+                            .observe(this, { userData ->
+                                if (userData != null) {
+                                    showLoading(false)
+                                    Toast.makeText(
+                                        this,
+                                        "Hello, ${userData.name}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    val intent =
+                                        Intent(this@LoginActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                            })
                     } catch (e: ApiException) {
                         // Google Sign In failed, update UI appropriately
+                        showLoading(false)
+                        Toast.makeText(this, "Login Failed, $e", Toast.LENGTH_LONG).show()
                         Log.w(TAG, "Google sign in failed", e)
                     }
                 }
@@ -55,6 +80,14 @@ class LoginActivity : AppCompatActivity() {
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBarLogin.visibility = View.VISIBLE
+        } else {
+            binding.progressBarLogin.visibility = View.GONE
+        }
     }
 
 }

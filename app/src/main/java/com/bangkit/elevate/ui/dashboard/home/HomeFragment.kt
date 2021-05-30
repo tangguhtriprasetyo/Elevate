2bdaebb8063c262f5799b41bf5106d2c2da8f26f
Tangguh Tri Prasetyo <tangguhtriprasetyo@gmail.com>
2021-05-31 02:06:42 +0700

    Create Get Pdf and Image From Local File

--- app/src/main/java/com/bangkit/elevate/ui/dashboard/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/elevate/ui/dashboard/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.elevate.databinding.FragmentHomeBinding
 
@@ -28,13 +26,12 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
 //        val textView: TextView = binding.textHome
 //        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/bangkit/elevate/ui/dashboard/ideator/AddIdeaFragment.kt
+++ app/src/main/java/com/bangkit/elevate/ui/dashboard/ideator/AddIdeaFragment.kt
@@ -1,20 +1,90 @@
 package com.bangkit.elevate.ui.dashboard.ideator
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.bangkit.elevate.R
+import com.bangkit.elevate.databinding.FragmentAddIdeaBinding
+import java.io.File
 
 
 class AddIdeaFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddIdeaBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_idea, container, false)
+        binding = FragmentAddIdeaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val getImage =
+            registerForActivityResult(ActivityResultContracts.OpenDocument()) { uriImage ->
+                if (uriImage.path != null) {
+
+                    var imgPath = uriImage.path
+                    val imgCut = imgPath?.lastIndexOf('/')
+                    if (imgCut != -1) {
+                        imgPath = imgCut?.plus(1)?.let { imgPath?.substring(it) }
+                    }
+                    with(binding) {
+                        titleBrowsePhoto.visibility = View.GONE
+                        photoAfterUpload.visibility = View.VISIBLE
+                        titleImgAfterUpload.visibility = View.VISIBLE
+
+                        titleImgAfterUpload.text = imgPath
+                        photoAfterUpload.setImageURI(uriImage)
+                    }
+                }
+            }
+
+        val getPdf = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uriPdf ->
+
+            if (uriPdf.path != null) {
+                val file = Uri.fromFile(File(uriPdf.path.toString()))
+                var path = uriPdf.path
+                val cut = path?.lastIndexOf('/')
+                if (cut != -1) {
+                    path = cut?.plus(1)?.let { path?.substring(it) }
+                }
+
+                with(binding) {
+                    icProposalAfterUpload.visibility = View.VISIBLE
+                    titleBrowseProposal.visibility = View.VISIBLE
+                    titleAfterUpload.visibility = View.VISIBLE
+                    titleAfterUpload.text = path
+
+                    Log.d("getPdfPath: ", uriPdf.path.toString())
+                    Log.d("getPdfPathSegment: ", uriPdf.pathSegments.toString())
+                    Log.d("getPdfLastPathSegment: ", uriPdf.lastPathSegment.toString())
+                    Log.d("getPdfLastPathSegment: ", uriPdf.lastPathSegment.toString())
+                    Log.d("getUserInfo: ", uriPdf.userInfo.toString())
+                    Log.d("encodedPath: ", uriPdf.encodedPath.toString())
+                    Log.d("encodedQuery: ", uriPdf.encodedQuery.toString())
+                    Log.d("queryParameterNames: ", uriPdf.queryParameterNames.toString())
+                    Log.d("scheme: ", uriPdf.scheme.toString())
+                    Log.d("schemeSpecificPart: ", uriPdf.schemeSpecificPart.toString())
+                    Log.d("File: ", file.toString())
+                }
+            }
+        }
+
+        binding.titleBrowseProposal.setOnClickListener {
+            getPdf.launch(arrayOf("application/pdf"))
+        }
+
+        binding.titleBrowsePhoto.setOnClickListener {
+            getImage.launch(arrayOf("image/*"))
+        }
+
     }
 }
